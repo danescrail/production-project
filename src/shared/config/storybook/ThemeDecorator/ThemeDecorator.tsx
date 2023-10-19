64712fdf91ad3cd79abda481891e897b3dadb3fe
danescrail <cap3347@mail.ru>
2023-10-19 22:33:41 +0400

    fix error lint:ts

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,11 @@
-import { StoryFn} from '@storybook/react';
+/* eslint-disable react/display-name */
+import { StoryFn } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => {
-  return (
-    <div className={`app ${theme}`}>
-        <StoryComponent />
-    </div>
-  );
+    return (
+        <div className={`app ${theme}`}>
+            <StoryComponent />
+        </div>
+    );
 };
