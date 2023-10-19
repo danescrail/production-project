33a5a201259d49d2fd6c1c3ff1fd8a0ab9318d78
danescrail <cap3347@mail.ru>
2023-10-19 23:57:52 +0400

    fix conflic version storybook and loki

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,9 +1,8 @@
-import type { Preview } from "@storybook/react";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 
-const preview: Preview = {
+const preview = {
     parameters: {
         actions: { argTypesRegex: "^on[A-Z].*" },
         controls: {
