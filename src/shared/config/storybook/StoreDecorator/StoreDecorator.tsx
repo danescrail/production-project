41ae70c6533dc0bdd7c4a3863dc294320e33cc63
danescrail <cap3347@mail.ru>
2023-11-06 20:11:16 +0400

    fix StoreDecorator for asyncReducer(storeManager)

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { StoryFn } from "@storybook/react";
-import { DeepPartial } from "@reduxjs/toolkit";
+import { DeepPartial, ReducersMapObject } from "@reduxjs/toolkit";
 import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { loginReducer } from "features/AuthByUsername/model/slice/loginSlice";
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: StoryFn) => {
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer
+}
+
+export const StoreDecorator = (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+) => (StoryComponent: StoryFn) => {
     return (
-        <StoreProvider initialState={state}>
+        <StoreProvider initialState={state} asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}>
             <StoryComponent />
         </StoreProvider>
     )
